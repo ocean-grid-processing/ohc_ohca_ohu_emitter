9483c2d119a8dfa4c9c642fa7682187ac848281b
Katie Mills <you@example.com>
2026-08-12 12:22:40 -0600

    run config rc for prod_260810

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
-for level in 15_20 15_300 300_700 700_1850 1800_1850
-do
-  cp /scratch/alpine/wimi7695/validation/argo_ohc_Global_2004_2024_${level}/derive_LocalGP_2004_2024_lev${level}.nc data/.
-done
-
-sbatch combine.slurm
+tag=prod_260810
+sbatch combine.slurm $tag
